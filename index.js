bb8e70d2c1d23c5fd155def0f2f55222fc74c288
belu gavito <mbgavito@gmail.com>
2022-02-06 14:04:10 -0300

    iniciando servidor en express

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fs = require("fs");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const productos = fs.readFileSync("./productos.txt", "utf-8");
+const arrayProductos = JSON.parse(productos);
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (arrayProductos.length - 0)) + 0;
+}
+
+
+app.get("/", (req, res) => {
+  res.send(`<h1 style="color:orange;"> Bienvenidos a Express </h1>
+  <hr>
+  <h2 style="color:coral;"> Rutas: </h2>
+ <ul>
+  <li>/productos</li>
+  <li>/productosRandom</li>
+  <ul>
+  `);
+
+});
+
+app.get("/productos", (req, res) => {
+  res.send(arrayProductos);
+});
+
+app.get("/productosRandom", (req, res) => {
+  res.send(arrayProductos[getRandom()]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor activo en el port ${PORT}`);
+});
